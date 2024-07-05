26a1ad3d593305c5e85dadb6a6f8f94f9601bf80
Wali Shah <walishahllc@gmail.com>
2024-07-05 16:02:54 +0500

    Loop

---
+++ app.js
@@ -0,0 +1,55 @@
+// print 1 to 10 counting using for loop
+// start    Ending  update
+// for (let i = 1; i <= 10; i++) {
+// console.log(i);
+// }
+
+// for(initialisation;condition;updation){
+//do something
+// }
+
+// loop: Used to iterate(repeat) a piece of code
+
+//backward loop
+
+// for (let w = 10; w >= 1; w--) {
+//     console.log(w);
+// }
+
+for (let w = 10; w >= 1; w = w - 3) {
+    console.log(w);
+}
+
+// Print all odd numbers
+
+// for (let o = 2; o <= 20; o = o + 2) {
+//     console.log(o);
+// } 2 ka table
+
+// for (let j = 3; j <= 30; j = j + 3) {
+//     console.log(j);
+// } 3 ka table
+
+// Print all even no 2 to 40;
+
+// for (let e = 2; e <= 40; e = e + 2) {
+//     console.log(e)
+// }
+
+// infinit loop very harmfull for our website
+
+// example
+
+// for (let p = 1; p >= 0; p++) {
+//     console.log(p)
+// }
+
+// for (let i = 5; i >= 4; i++) {
+//     console.log(i)
+// }
+
+// Print 5 ka table using for loop
+
+for (let i = 5; 5 <= 50; i = i + 5) {
+    console.log(i)
+}
